d6e30528d8ea27b62d06061eb518a6533a278335
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-10 05:50:31 -0400

    Update greeting-card-solution.py

--- greeting-card-solution.py
+++ greeting-card-solution.py
@@ -1,1 +1,12 @@
+# Mike Jones
+# 05 SEP 20XX
+# f-String Custom Greeting Card
+
+name = input("Enter the recipient's name: ")
+occasion = input("Enter the occasion: ")
+message = input("Enter a custom message: ")
+
+print(f"Dear {name},\n\nHappy {occasion}!\n{message}\n\nBest wishes,\nYour Friend")
+
+
 
